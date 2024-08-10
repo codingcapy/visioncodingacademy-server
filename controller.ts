7ea2196e0f7dda71998398cfedbbd2f61e595600
Paul K <spkim0921@gmail.com>
2024-08-10 15:44:10 -0700

    3rd commit
    implementing UUID in controller.ts

--- controller.ts
+++ controller.ts
@@ -5,6 +5,11 @@ Version: 1.0.0
 description: controller for http webserver app for Vision Coding Academy
  */
 
-export async function createUser(){
+import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
+export async function createUser(req:Request, res:Response){
+    const user_id = uuidv4();
+    console.log(user_id)
+    res.status(200).send("success")
 }
