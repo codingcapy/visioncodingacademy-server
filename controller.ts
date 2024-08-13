577d94997429bfd2d7d374a68a922e948faa5b2b
Paul K <spkim0921@gmail.com>
2024-08-13 13:53:15 -0700

    14th commit
    Implementing data checks and bcrypt in createUser function in controller.ts

--- controller.ts
+++ controller.ts
@@ -9,16 +9,38 @@ import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { db, questions, users } from './connect';
 import dotenv from "dotenv";
+import { eq } from 'drizzle-orm';
+import bcrypt from "bcrypt"
 dotenv.config()
 
+const saltRounds = 10;
+
 export async function createUser(req: Request, res: Response) {
+    const { email, username, password } = req.body;
+    if (username.length > 32) {
+        return res.json({ success: false, message: "Username max char limit is 32" });
+    }
+    if (password.length > 80) {
+        return res.json({ success: false, message: "password max char limit is 80" });
+    }
+    if (email.length > 255) {
+        return res.json({ success: false, message: "email max char limit is 255" });
+    }
     try {
-        const { email, username, password } = req.body;
+        const usernameQuery = await db.select().from(users).where(eq(users.username, username))
+        if (usernameQuery.length > 0) {
+            return res.json({ success: false, message: "Username already exists" });
+        };
+        const emailQuery = await db.select().from(users).where(eq(users.email, email))
+        if (emailQuery.length > 0) {
+            return res.json({ success: false, message: "An account associated with this email already exists" });
+        };
+        const encrypted = await bcrypt.hash(password, saltRounds);
         const user_id = uuidv4();
         const now = new Date();
         const timestamp = now.toISOString();
-        await db.insert(users).values({ user_id, email, username, password, created_at: timestamp })
-        res.status(200).json({ success: true, message: "User created successfully" })
+        await db.insert(users).values({ user_id, email, username, password:encrypted, created_at: timestamp })
+        res.status(200).json({ success: true, message: "Success! Redirecting..." })
     }
     catch (err) {
         console.log(err)
